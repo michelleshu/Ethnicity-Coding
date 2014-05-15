0e8ba102311cc2e21b101b8e22765b22e4fd1bfd
Michelle Shu <simplevity@gmail.com>
2014-05-15 00:50:31 -0400

    remove commas

--- removeCommas.py
+++ removeCommas.py
@@ -6,12 +6,15 @@ import os
 def removeCommas(fileDir):
     fileNames = os.listdir(fileDir)
     for fileName in fileNames:
-        with open(fileName, mode="rU") as inFile:
-            filereader = csv.reader(infile, dialect=csv.excel_tab)
-            with open("_" + fileName, mode="w") as outfile:
+        with open(fileDir + '/' + fileName, mode="rU") as inFile:
+            filereader = csv.reader(inFile, dialect=csv.excel_tab)
+            with open(fileDir + '/_' + fileName, mode="w") as outfile:
                 for rows in filereader:
                     filewriter = csv.writer(outfile, delimiter='"')
                     filewriter.writerows(rows)
+            filewriter.close()
+            filereader.close()
+        os.remove(fileDir + '/' + fileName)
 
 def main():
     removeCommas(sys.argv[1])
